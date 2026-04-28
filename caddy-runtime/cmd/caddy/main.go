57e61986aaa715bfa480b0c4c0eacdd6296828a9
simpcl <simpcl2008@gmail.com>
2026-04-28 16:07:38 +0800

    Add caddy-runtime

---
+++ caddy-runtime/cmd/caddy/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	caddycmd "github.com/caddyserver/caddy/v2/cmd"
+
+	_ "github.com/agent-guide/caddy-agent-gateway/admin"
+	_ "github.com/agent-guide/caddy-agent-gateway/api"
+	_ "github.com/agent-guide/caddy-agent-gateway/api/llmapi/anthropic"
+	_ "github.com/agent-guide/caddy-agent-gateway/api/llmapi/openai"
+	_ "github.com/agent-guide/caddy-agent-gateway/cliauth/authenticator"
+	_ "github.com/agent-guide/caddy-agent-gateway/gateway"
+	_ "github.com/agent-guide/caddy-agent-gateway/llm/provider/anthropic"
+	_ "github.com/agent-guide/caddy-agent-gateway/llm/provider/gemini"
+	_ "github.com/agent-guide/caddy-agent-gateway/llm/provider/ollama"
+	_ "github.com/agent-guide/caddy-agent-gateway/llm/provider/openai"
+	_ "github.com/agent-guide/caddy-agent-gateway/llm/provider/openrouter"
+	_ "github.com/agent-guide/caddy-agent-gateway/llm/provider/zhipu"
+	_ "github.com/agent-guide/caddy-x402pay"
+	_ "github.com/caddyserver/caddy/v2/modules/standard"
+)
+
+func main() {
+	caddycmd.Main()
+}
